ca46b79d92d9154f1aaffd69dfe82cf783d0ba62
billyfarrell95 <billyfarrell95@gmail.com>
2022-10-10 09:52:47 -0500

    Pulls data and saves/updates result card

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let searchBar = document.querySelector("#search-bar");
-let resultCard = document.querySelector(".results");
-let resultName = document.querySelector("")
+let resultCard = document.querySelector(".result-card");
+let defaultResultsMsg = document.querySelector(".default-results-msg");
+let resultName = document.querySelector(".name");
+let resultSymbol = document.querySelector(".symbol");
+let resultPrice = document.querySelector(".price");
+let resultMarketCap = document.querySelector(".market-cap");
+let resultSupply = document.querySelector(".supply");
+
+resultCard.style.display = "none";
 
 async function search() {
     try {
@@ -8,19 +15,26 @@ async function search() {
         let response = await fetch(`https://api.coincap.io/v2/assets/${searchValue}`);
         let asset = await response.json();
         generateCard(asset);
+        resultCard.style.display = "block";
+        defaultResultsMsg.style.display = "none";
     }
     catch {
         console.error(error);
     }
 }
 
-
 function generateCard(searchResult) {
     let cardData = {
         name: searchResult.data.name,
         symbol: searchResult.data.symbol,
-        priceUSD: searchResult.data.priceUSD,
+        priceUSD: searchResult.data.priceUsd,
         marketCap: searchResult.data.marketCapUsd,
         supply: searchResult.data.supply
     };
+    /* console.log(cardData); */
+    resultName.innerHTML = cardData.name;
+    resultSymbol.innerHTML = cardData.symbol;
+    resultPrice.innerHTML = cardData.priceUSD;
+    resultMarketCap.innerHTML = cardData.marketCap;
+    resultSupply.innerHTML = cardData.supply;
 }
